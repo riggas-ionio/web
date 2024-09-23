a40f67cb0fc75c5c5a91faae33a3b6a6f6358644
Konstantinos Lygkouris <103383737+Lygkouris@users.noreply.github.com>
2024-09-23 23:05:48 +0300

    Update index.php

--- docker-compose/php/index.php
+++ docker-compose/php/index.php
@@ -1,32 +1,20 @@
 <?php
 // Database connection parameters
 $servername = "di_inter_tech_mysql"; // Change this to your database server name if necessary
-$username = "webuser"; // Change this to your database username
-$password = "webpass"; // Change this to your database password
-$dbname = "di_internet_technologies_project"; // Change this to your database name
+$host = "localhost";
+$dbname = "project";
+$username = "root";
+$password = "";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
 }
 
-// Query to fetch quotes from the database
-$sql = "SELECT value FROM test";
-
-$result = $conn->query($sql);
-
-// Check if there are any rows returned
-if ($result->num_rows > 0) {
-    // Output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo $row["value"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-// Close connection
-$conn->close();
+return $mysqli;
 ?>
